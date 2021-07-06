0a1902e5b9eeff302fdc9cf3f3aafbba604543e2
ajmyquira <ajmyquira@DESKTOP-C4LRU4V.localdomain>
2021-07-05 21:58:54 -0500

    multipleClientsConexion

---
+++ build/CMakeFiles/repositorio.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/repositorio.dir/source/repositorio.cpp.o"
+  "repositorio"
+  "repositorio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/repositorio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
